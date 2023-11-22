8f3db84527ef747e1d0b6a6e637d98e660989295
Yavuz Ulgar <yavuzulgar@Yavuz-MacBook-Air.local>
2023-11-22 11:43:58 +0300

    simple quiz ready

---
+++ CountryFlag_Project2/CountryFlag_Project2App.swift
@@ -0,0 +1,17 @@
+//
+//  CountryFlag_Project2App.swift
+//  CountryFlag_Project2
+//
+//  Created by Yavuz Ulgar on 2.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CountryFlag_Project2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
